1a595db01e02b26323842de94ebc290360c5b15c
Prelang Builder <builder@prelang.com>
2015-03-05 10:26:01 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,4 +1,8 @@
 class HousesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_house, only: [:show, :edit, :update, :destroy]
 
   # GET /houses
